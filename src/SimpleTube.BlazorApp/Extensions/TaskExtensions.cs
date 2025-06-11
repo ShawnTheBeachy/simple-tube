9c75907cfb1a3d4680e1768dd28e4c523e87eea9
Shawn Beachy <shawnthebeachy@outlook.com>
2025-06-11 09:35:42 -0400

    Allow switching between channels and subscriptions.

---
+++ src/SimpleTube.BlazorApp/Extensions/TaskExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleTube.BlazorApp.Extensions;
+
+internal static class TaskExtensions
+{
+    public static async void FireAndForget(
+        this ValueTask task,
+        Action<Exception>? errorHandler = null
+    )
+    {
+        try
+        {
+            await task;
+        }
+        catch (Exception exception)
+        {
+            errorHandler?.Invoke(exception);
+        }
+    }
+
+    public static async void FireAndForget(this Task task, Action<Exception>? errorHandler = null)
+    {
+        try
+        {
+            await task;
+        }
+        catch (Exception exception)
+        {
+            errorHandler?.Invoke(exception);
+        }
+    }
+}
--- src/SimpleTube.BlazorApp/Pages/ChannelsPage/ChannelsPage.razor.cs
+++ src/SimpleTube.BlazorApp/Pages/ChannelsPage/ChannelsPage.razor.cs
@@ -1,6 +1,9 @@
 ﻿using System.Net.Http.Json;
 using System.Text.Json.Serialization;
 using BlazorComponentBus;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using SimpleTube.BlazorApp.Extensions;
 using SimpleTube.BlazorApp.Infrastructure.Api;
 using SimpleTube.BlazorApp.Messages;
 
@@ -9,15 +12,22 @@ namespace SimpleTube.BlazorApp.Pages.ChannelsPage;
 public sealed partial class ChannelsPage : IAsyncDisposable
 {
     private readonly IComponentBus _bus;
-    private readonly CancellationTokenSource _cancellation = new();
+    private CancellationTokenSource _cancellation = new();
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly NavigationManager _navigation;
 
     private IReadOnlyList<Channel> Channels { get; set; } = [];
+    private View CurrentView { get; set; } = View.AllChannels;
 
-    public ChannelsPage(IComponentBus bus, IHttpClientFactory httpClientFactory)
+    public ChannelsPage(
+        IComponentBus bus,
+        IHttpClientFactory httpClientFactory,
+        NavigationManager navigation
+    )
     {
         _bus = bus;
         _httpClientFactory = httpClientFactory;
+        _navigation = navigation;
     }
 
     public async ValueTask DisposeAsync()
@@ -25,12 +35,19 @@ public sealed partial class ChannelsPage : IAsyncDisposable
         await _cancellation.CancelAsync();
         _cancellation.Dispose();
         _bus.UnSubscribeFrom<SubscribedToChannelMessage>(OnSubscribedToChannel);
+        _navigation.LocationChanged -= OnLocationChanged;
     }
 
-    protected override async Task OnInitializedAsync()
+    protected override void OnInitialized()
     {
-        await Refresh(_cancellation.Token);
+        UpdateCurrentView();
+        _navigation.LocationChanged += OnLocationChanged;
         _bus.SubscribeTo<SubscribedToChannelMessage>(OnSubscribedToChannel);
+    }
+
+    private void OnLocationChanged(object? sender, LocationChangedEventArgs args)
+    {
+        UpdateCurrentView();
     }
 
     private async Task OnSubscribedToChannel(
@@ -53,15 +70,31 @@ public sealed partial class ChannelsPage : IAsyncDisposable
             .OrderBy(x => x.Name)
             .ToArray();
         await InvokeAsync(StateHasChanged);
-        await Refresh(cancellationToken);
+        await Refresh();
     }
 
-    private async Task Refresh(CancellationToken cancellationToken)
+    private async Task Refresh()
     {
+        await _cancellation.CancelAsync();
+        _cancellation.Dispose();
+        _cancellation = new CancellationTokenSource();
         var channels = await _httpClientFactory
             .CreateApiClient()
-            .GetFromJsonAsync<IReadOnlyList<Channel>>("channels", cancellationToken);
+            .GetFromJsonAsync<IReadOnlyList<Channel>>(
+                CurrentView == View.Subscriptions ? "subscriptions" : "channels",
+                _cancellation.Token
+            );
         Channels = channels!;
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private void UpdateCurrentView()
+    {
+        CurrentView =
+            _navigation.Uri.Split('/', StringSplitOptions.RemoveEmptyEntries)[^1] == "subscriptions"
+                ? View.Subscriptions
+                : View.AllChannels;
+        Refresh().FireAndForget();
     }
 
     private sealed record Channel
@@ -69,10 +102,17 @@ public sealed partial class ChannelsPage : IAsyncDisposable
         public required string Handle { get; init; }
         public required string Id { get; init; }
         public required string Name { get; init; }
+        public bool? Subscribed { get; init; }
         public required string Thumbnail { get; init; }
         public required int UnwatchedVideos { get; init; }
 
         [JsonPropertyName("$entity#url")]
         public required string Url { get; init; }
+    }
+
+    private enum View
+    {
+        AllChannels,
+        Subscriptions,
     }
 }
